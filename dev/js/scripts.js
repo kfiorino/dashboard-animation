7f0c4b9e1ea6ae90527179af3ef75a47b86c8e6e
kfiorino <63323923+kfiorino@users.noreply.github.com>
2020-11-15 19:39:28 -0500

    init

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,21 +1,2 @@
-import $ from "jquery";
-import { gsap } from "gsap";
-import { toggleAnimation } from './demo.js';
 
-var toggle = false;
 
-const mainTL = gsap.timeline({paused:true});
-mainTL.add(toggleAnimation());
-
-$("#svg-demo").on("click", function(){
-    console.log("working");
-    if(toggle === false){
-
-        mainTL.play();
-        toggle = true;
-
-    }else{
-        mainTL.reverse();
-       toggle = false;
-    }
-})
