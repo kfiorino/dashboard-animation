ea198682cc2f130d61254fb4f66740199e4c0ccc
kfiorino <63323923+kfiorino@users.noreply.github.com>
2020-11-17 16:26:47 -0500

    mid-line

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -6,7 +6,7 @@ gsap.registerPlugin(DrawSVGPlugin);
 const drawTL = gsap.timeline();
 export function drawAnimation(){
 
-   drawTL.from("#underline", {duration:2, drawSVG: "0%"});
+   drawTL.from("#mid-line", {duration:2, drawSVG: "0%"});
 
     return drawTL;
 }
