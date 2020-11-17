675f16a4d7ea17dedb316968fcb374258161d475
kfiorino <63323923+kfiorino@users.noreply.github.com>
2020-11-17 15:47:07 -0500

    bonus added

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,2 +1,7 @@
+import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
 
+gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin, GSDevTools);
 
