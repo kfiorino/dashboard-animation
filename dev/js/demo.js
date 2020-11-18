3fe9f78698210c0e21b3d706f2faf2ecdf2d1fa8
kfiorino <63323923+kfiorino@users.noreply.github.com>
2020-11-17 22:33:04 -0500

    logo draw and transform

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -6,7 +6,23 @@ gsap.registerPlugin(DrawSVGPlugin);
 const drawTL = gsap.timeline();
 export function drawAnimation(){
 
-   drawTL.from("#mid-line", {duration:2, drawSVG: "0%"});
+   drawTL.from("#outline", {duration:3, drawSVG: "0%"}, "start");
+   drawTL.from("#bottom-line", {duration:3, drawSVG: "0%"}, "start" );
+   drawTL.from("#shell-line1", {duration:3, drawSVG: "0%"}, "start");
+   drawTL.from("#shell-line2", {duration:3, drawSVG: "0%"}, "start");
+   drawTL.from("#mid-line", {duration:3, drawSVG: "0%"}, "start");
+   drawTL.from("#underline", {duration:3, drawSVG: "0%"}, "start");
+   drawTL.from("#krusty-krab", {duration:3, alpha: 0}, "start");
+
+   drawTL.to("#outlines", {duration:1, scale: .3, x: 10, y:10}, "next")
+   drawTL.to("#krusty-krab", {duration:1, scale: .3, x: 22, y:22}, "next")
+  
+  
+ 
+   
+
+   
+
 
     return drawTL;
-}
+} 
